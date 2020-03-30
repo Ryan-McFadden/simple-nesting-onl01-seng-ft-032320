a2ba00c84add0861d1d4b882e6dc30f820eb6772
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-30 22:31:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,8 +86,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+     
+programmer_hash[:yukihiro_matsumoto] = {
+  known_for: "Ruby"
+  language: ["LISP", "C"]
+}
 end
 
 def changing_alan
